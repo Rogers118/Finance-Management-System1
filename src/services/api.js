1cfe7f570ac7388215a35174b89eff706cb3f315
Rogers118 <150867362+Rogers118@users.noreply.github.com>
2025-11-27 18:03:35 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = process.env.REACT_APP_API_URL || 'https://github.com/Rogers118/Finance-Management-System1.git';
 
 const api = axios.create({
     baseURL: `${API_URL}/api`,
